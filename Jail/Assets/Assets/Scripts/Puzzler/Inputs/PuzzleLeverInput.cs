688f13919864fc5ea26917c89ba8999925761505
Guthen <33220603+Guthen@users.noreply.github.com>
2022-09-03 14:55:38 +0200

    [added] puzzler IO base

---
+++ Jail/Assets/Assets/Scripts/Puzzler/Inputs/PuzzleLeverInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Jail.Puzzler.Inputs
+{
+	public class PuzzleLeverInput : PuzzleBaseInput
+	{
+		bool isPlayerInFront = false;
+
+		void Update()
+		{
+			if ( !isPlayerInFront ) return;
+
+			if ( Input.GetButtonDown( "Interact" ) )
+				IsTriggered = !IsTriggered;
+		}
+
+		void OnTriggerEnter( Collider other )
+		{
+			isPlayerInFront = true;
+		}
+
+		void OnTriggerExit( Collider other )
+		{
+			isPlayerInFront = false;
+		}
+	}
+}
---
+++ Jail/Assets/Assets/Scripts/Puzzler/Outputs/PuzzleDoorOutput.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Jail.Puzzler.Outputs
+{
+	public class PuzzleDoorOutput : PuzzleBaseOutput
+	{
+		AutomaticSlider slider;
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			slider = GetComponent<AutomaticSlider>();
+			slider.enabled = false;
+		}
+
+		public override void OnInputsTriggered()
+		{
+			print( "Opening door!" );
+
+			slider.enabled = true;
+			slider.Reversed = false;
+		}
+
+		public override void OnInputsUnTriggered()
+		{
+			print( "Closing door!" );
+
+			slider.enabled = true;
+			slider.Reversed = true;
+		}
+	}
+}
