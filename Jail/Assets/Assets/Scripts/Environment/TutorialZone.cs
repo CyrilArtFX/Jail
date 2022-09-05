b769e01032dfba10b1d3059b897ab3a8d2336199
CyrilArtFX <ccrozat@artfx.fr>
2022-09-05 14:14:53 +0200

    [edited] tutorial zones

--- Jail/Assets/Assets/Scripts/Environment/TutorialZone.cs
+++ Jail/Assets/Assets/Scripts/Environment/TutorialZone.cs
@@ -5,73 +5,27 @@ using Jail.Utility;
 
 public class TutorialZone : MonoBehaviour
 {
-    [SerializeField] LayerMask detectedLayers = -1;
-    [SerializeField, TextArea] string messageToDisplay;
-    [SerializeField] TextMeshProUGUI textToDisplay;
-
-    List<Collider> colliders = new List<Collider>();
-
-    void Awake()
-    {
-        enabled = false;
-        textToDisplay.gameObject.SetActive(false);
-    }
-
-    void FixedUpdate()
-    {
-        for (int i = 0; i < colliders.Count; i++)
-        {
-            Collider collider = colliders[i];
-            if (!collider || !collider.gameObject.activeInHierarchy)
-            {
-                colliders.RemoveAt(i--);
-                if (colliders.Count == 0)
-                {
-                    Exit();
-                    enabled = false;
-                }
-            }
-        }
-    }
+    [SerializeField] 
+    LayerMask detectedLayers = -1;
+    [SerializeField, TextArea] 
+    string messageToDisplay;
+    [SerializeField] 
+    TextMeshProUGUI textToDisplay;
 
     void OnTriggerEnter(Collider other)
     {
-        if (LayerMaskUtils.HasLayer(detectedLayers, other.gameObject.layer))
-        {
-            if (colliders.Count == 0)
-            {
-                Enter();
-                enabled = true;
-            }
-            colliders.Add(other);
-        }
+        if (!LayerMaskUtils.HasLayer(detectedLayers, other.gameObject.layer))
+            return;
+
+        Enter();
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (LayerMaskUtils.HasLayer(detectedLayers, other.gameObject.layer))
-        {
-            if (colliders.Remove(other) && colliders.Count == 0)
-            {
-                Exit();
-                enabled = false;
-            }
-        }
-    }
-
-    void OnDisable()
-    {
-#if UNITY_EDITOR
-        if (enabled && gameObject.activeInHierarchy)
-        {
+        if (!LayerMaskUtils.HasLayer(detectedLayers, other.gameObject.layer))
             return;
-        }
-#endif
-        if (colliders.Count > 0)
-        {
-            colliders.Clear();
-            Exit();
-        }
+
+        Exit();
     }
 
     void Enter()
