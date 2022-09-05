be8549e2f80e9eb0a52ccae9727a7c6101b077fb
Guthen <33220603+Guthen@users.noreply.github.com>
2022-09-05 11:00:51 +0200

    [dev] added coding style

---
+++ Jail/Assets/Assets/Scripts/CodingStyle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Jail
+{
+    public class CodingStyle
+    {
+        [SerializeField] 
+        bool isPrivate;
+        bool IsPublic => !isPrivate;
+
+        void Awake()
+        {
+            bool local_var = false;
+            if (!local_var) return;
+            
+            if (local_var)
+            {
+                Console.WriteLine("hey");
+            }
+            else
+            {
+                //Make a spirit
+                //  make a spirit
+            }
+        }
+
+        public void DoThing()
+        {
+            //  code
+            Component comp = default;
+            if (comp != null) return;
+
+            /*bool has_hit = Physics.Raycast(
+                body.position,
+                -Vector3.up,
+                out RaycastHit hit,
+                probeDistance,
+                probeMask,
+                QueryTriggerInteraction.Ignore
+            );*/
+        }
+    }
+}
