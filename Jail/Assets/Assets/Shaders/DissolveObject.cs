456249f512b9654e5af436f2dbb1aeb116854855
CyrilArtFX <ccrozat@artfx.fr>
2022-09-06 16:03:58 +0200

    [added] set urp and added dissolve shader and script

---
+++ Jail/Assets/Assets/Shaders/DissolveObject.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveObject : MonoBehaviour
+{
+    List<Renderer> renderers = new List<Renderer>();
+
+    [SerializeField]
+    float dissolveTime;
+
+    bool dissolveStarted = false;
+    float dissolveValue = 0f;
+
+    void Awake()
+    {
+        GetAllRenderersOfChilds(this.transform);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("l")) Dissolve();
+
+        if(dissolveStarted)
+        {
+            dissolveValue += Time.deltaTime / dissolveTime;
+            foreach(Renderer renderer in renderers)
+            {
+                renderer.material.SetFloat("Vector1_7d1cd5e28bfe440ea3a8058edcc3457f", dissolveValue);
+            }
+
+            if(dissolveValue >= 1)
+            {
+                dissolveStarted = false;
+            }
+        }
+    }
+
+    public void Dissolve()
+    {
+        dissolveStarted = true;
+    }
+
+    private void GetAllRenderersOfChilds(Transform objectToCheck)
+    {
+        if (objectToCheck.gameObject.TryGetComponent(out Renderer newRenderer))
+        {
+            renderers.Add(newRenderer);
+        }
+
+        if (objectToCheck.childCount == 0) return;
+
+        for(int i = 0; i < objectToCheck.childCount; i++)
+        {
+            GetAllRenderersOfChilds(objectToCheck.GetChild(i));
+        }
+    }
+}
