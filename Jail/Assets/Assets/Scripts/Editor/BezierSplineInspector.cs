97433c34e60d2f0751d4a3571c7a4fa610cdecef
Guthen <33220603+Guthen@users.noreply.github.com>
2022-09-08 15:36:48 +0200

    [added] custom Bezier Spline

---
+++ Jail/Assets/Assets/Scripts/Editor/BezierSplineInspector.cs
@@ -0,0 +1,177 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using Jail.Utility.Bezier;
+
+namespace Jail.Debug
+{
+    [CustomEditor(typeof(BezierSpline))]
+    public class BezierSplineInspector : Editor
+    {
+        const int STEPS_PER_CURVE = 10;
+
+        const float HANDLE_SIZE = 0.04f;
+        const float PICK_SIZE = 0.06f;
+
+        int selectedId = -1;
+
+        BezierSpline spline;
+        Transform handleTransform;
+        Quaternion handleRotation;
+
+        Color[] modesColors =
+        {
+            Color.white,
+            Color.yellow,
+            Color.cyan,
+        };
+
+        public override void OnInspectorGUI()
+        {
+            spline = (BezierSpline) target;
+
+            //  show selected point
+            if (selectedId >= 0 && selectedId <= spline.ControlPointCount)
+            {
+                DrawSelectedPointInspector();
+            }
+
+            //  add curve button
+            if (GUILayout.Button("Add Curve"))
+            {
+                //  record undo
+                Undo.RecordObject(spline, "Add Curve");
+
+                //  set object as dirty
+                EditorUtility.SetDirty(spline);
+
+                //  add curve
+                spline.AddCurve();
+            }
+        }
+
+        void DrawSelectedPointInspector()
+        {
+            GUILayout.Label("Selected Point");
+
+            //  show position
+            EditorGUI.BeginChangeCheck();
+            Vector3 point = EditorGUILayout.Vector3Field("Position", spline.GetControlPoint(selectedId));
+            if (EditorGUI.EndChangeCheck())
+            {
+                //  record undo
+                Undo.RecordObject(spline, "Move Point");
+
+                //  set object as dirty
+                EditorUtility.SetDirty(spline);
+
+                //  change point
+                spline.SetControlPoint(selectedId, point);
+            }
+
+            //  show mode
+            EditorGUI.BeginChangeCheck();
+            BezierControlPointMode mode = (BezierControlPointMode) EditorGUILayout.EnumPopup("Mode", spline.GetControlPointMode(selectedId));
+            if (EditorGUI.EndChangeCheck())
+            {
+                //  record undo
+                Undo.RecordObject(spline, "Change Point Mode");
+
+                //  set object as dirty
+                EditorUtility.SetDirty(spline);
+
+                //  change point mode
+                spline.SetControlPointMode(selectedId, mode);
+            }
+        }
+
+        void OnSceneGUI()
+        {
+            spline = (BezierSpline) target;
+
+            //  get transform
+            handleTransform = spline.transform;
+
+            //  get handle rotation depending on tool's pivot mode
+            handleRotation = handleTransform.rotation;
+            if (Tools.pivotRotation == PivotRotation.Global)
+                handleRotation = Quaternion.identity;
+
+            //  draw spline
+            Vector3 last_point = DoHandlePoint(0);
+            for (int i = 1; i < spline.ControlPointCount; i += 3)
+            {
+                //  get points
+                Vector3 a = DoHandlePoint(i), 
+                        b = DoHandlePoint(i + 1), 
+                        c = DoHandlePoint(i + 2);
+                
+                //  draw lines
+                Handles.color = Color.gray;
+                Handles.DrawLine(last_point, a);
+                Handles.DrawLine(b, c);
+
+                //  draw bezier curve
+                Handles.DrawBezier(last_point, c, a, b, Color.white, null, 3.0f);
+                last_point = c;
+            }
+
+            //  draw directions
+            //ShowDirections();
+        }
+
+        void ShowDirections()
+        {
+            float steps = STEPS_PER_CURVE * spline.CurveCount;
+
+            //  change color
+            Handles.color = Color.green;
+
+            //  draw first direction
+            Vector3 point = spline.GetPoint(0.0f);
+            Handles.DrawLine(point, point + spline.GetDirection(0.0f));
+
+            //  draw all directions
+            for (int i = 1; i <= steps; i++)
+            {
+                float next_step = i / steps;
+                point = spline.GetPoint(next_step);
+                Handles.DrawLine(point, point + spline.GetDirection(next_step));
+            }
+        }
+
+        Vector3 DoHandlePoint(int id)
+        {
+            Vector3 point = handleTransform.TransformPoint(spline.GetControlPoint(id));
+
+            //  attempt a selection
+            float scale = HandleUtility.GetHandleSize(point);
+            Handles.color = modesColors[(int) spline.GetControlPointMode(id)];
+            if (Handles.Button(point, handleRotation, scale * HANDLE_SIZE, scale * PICK_SIZE, Handles.DotHandleCap))
+            {
+                selectedId = id;
+                Repaint();  //  update inspector
+            }
+
+            //  move selected
+            if (selectedId == id)
+            {
+                EditorGUI.BeginChangeCheck();
+                point = Handles.DoPositionHandle(point, handleRotation);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    //  record undo
+                    Undo.RecordObject(spline, "Move Point");
+                
+                    //  set object as dirty
+                    EditorUtility.SetDirty(spline);
+
+                    //  change point
+                    spline.SetControlPoint(id, handleTransform.InverseTransformPoint(point));
+                }
+            }
+
+            return point;
+        }
+    }
+}
---
+++ Jail/Assets/Assets/Scripts/Utility/Bezier/BezierSplineChainer.cs
@@ -0,0 +1,128 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Jail.Utility.Bezier;
+
+namespace Jail.Environment
+{
+    [ExecuteInEditMode]
+    public class BezierSplineChainer : MonoBehaviour
+    {
+        [SerializeField]
+        BezierSpline spline;
+        [SerializeField]
+        GameObject prefab;
+        [SerializeField]
+        float scale = .2f;
+
+        [SerializeField]
+        float stepSize = .5f;
+
+        List<Transform> items = new List<Transform>();
+        bool toUpdate = false;
+
+        public void DoUpdate()
+        {
+            if (!spline || !prefab) return;
+            if (scale <= 0.0f || stepSize <= 0.0f) return;
+
+            //  place items
+            int i = 0;
+            float dist = 0.0f;
+            while (dist < spline.Length)
+            {
+                //  get transform item
+                Transform item;
+                if (items.Count <= i)
+                {
+                    //  create object
+                    item = Instantiate(prefab, transform).transform;
+
+                    //  register item
+                    items.Add(item);
+                }
+                else
+                {
+                    //  re-use last item
+                    item = items[i];
+                }
+
+                //  compute ratio
+                float dist_ratio = dist / spline.Length;
+
+                //  set position
+                Vector3 point = spline.GetPoint(dist_ratio);
+                item.position = point;
+
+                //  rotate
+                item.LookAt(point + spline.GetDirection(dist_ratio));
+                if (i % 2 == 0)
+                {
+                    //  give a chain look
+                    item.Rotate(Vector3.forward, 90.0f);
+                }
+
+                //  scale
+                item.localScale = new Vector3(scale, scale, scale);
+
+                //  increase dist
+                dist += stepSize;
+                i++;
+            }
+
+            //  remove last items
+            for (int j = i; j < items.Count; j++)
+            {
+                Transform item = items[j];
+                if (item != null)
+                {
+                    #if UNITY_EDITOR
+                        DestroyImmediate(item.gameObject);
+                    #else
+                        Destroy(item.gameObject);
+                    #endif
+                }
+            }
+            items.RemoveRange(i, items.Count - i);
+        }
+
+        public void QueueUpdate()
+        {
+            toUpdate = true;
+        }
+
+        void SetupSpline()
+        {
+            //  try to retrieve spline automatically
+            TryGetComponent(out spline);
+
+            //  update when spline is edited
+            spline.OnSplineEdited.AddListener(QueueUpdate);
+        }
+
+        void Awake()
+        {
+            SetupSpline();
+            QueueUpdate();
+        }
+
+        void OnValidate()
+        {
+            //  get children as items
+            items = transform.Cast<Transform>().ToList();
+
+            SetupSpline();
+            QueueUpdate();
+        }
+
+        void Update()
+        {
+            if (toUpdate)
+            {
+                DoUpdate();
+                toUpdate = false;
+            }
+        }
+    }
+}
