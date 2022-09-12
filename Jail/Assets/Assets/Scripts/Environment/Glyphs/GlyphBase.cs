9de010f0f566387499c0ddb89c199bdd3d398138
Guthen <33220603+Guthen@users.noreply.github.com>
2022-09-12 19:58:43 +0200

    [removed] useless glyph code

--- Jail/Assets/Assets/Scripts/Environment/Glyphs/GlyphBase.cs
+++ Jail/Assets/Assets/Scripts/Environment/Glyphs/GlyphBase.cs
@@ -20,35 +20,6 @@ namespace Jail.Environment.Glyphs
 
         void Update()
         {
-            //  get direction
-            /*Vector2 direction = new Vector2(target.position.z - transform.position.z, target.position.y - transform.position.y);  //  flat depth
-            float dist_ratio = direction.sqrMagnitude / distToSqr;
-
-            //  get gradient evaluation time
-            if ((Player.instance.IsSpirit || Player.instance.IsSpiritReturning) && dist_ratio <= 1.0f)
-            {
-                t = Mathf.MoveTowards(t, 1.0f - dist_ratio, Time.deltaTime * data.smoothFactor);
-                //hasDetectedTarget = true;
-            }
-            else
-            {
-                t = Mathf.MoveTowards(t, 0.0f, Time.deltaTime * data.smoothFactor);
-                //hasDetectedTarget = false;
-            }
-
-            //  evaluate color from gradient
-            Color new_color = data.gradient.Evaluate(t);
-            *//*if (data.isRGBGamer)
-            {
-                float hue = (transform.position.z + transform.position.y + Time.time) % 1.0f;
-                if (data.isDistanceDependent)
-                {
-                    hue *= t;
-                }
-                new_color = Color.HSVToRGB(hue, 1.0f, 1.0f);
-            }*//*
-            ApplyColor(new_color);*/
-
             Color priority_color = data.gradient.Evaluate(0.0f);
             int priority_id = int.MaxValue;
             foreach (GlyphDataTarget target in data.targets)
@@ -110,20 +81,6 @@ namespace Jail.Environment.Glyphs
             //  apply color
             ApplyColor(smoothPriorityColor);
         }
-
-        /*void OnDrawGizmosSelected()
-        {
-            if (data == null) return;
-
-            Gizmos.color = hasDetectedTarget ? Color.green : Color.red;
-
-            Vector3 position = transform.position;
-            if (target != null)
-            {
-                position.x = target.position.x;
-            }
-            Gizmos.DrawWireSphere(position, data.distance);
-        }*/
 
         public virtual void ApplyColor(Color color) {}
     }
