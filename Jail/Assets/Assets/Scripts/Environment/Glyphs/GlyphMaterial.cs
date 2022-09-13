ef1a3b5fc366f24f20b52ef7dd9205bdda3d57c4
Guthen <33220603+Guthen@users.noreply.github.com>
2022-09-13 11:06:36 +0200

    [added] glyph material & script

---
+++ Jail/Assets/Assets/Scripts/Environment/Glyphs/GlyphMaterial.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Jail.Environment.Glyphs
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class GlyphMaterial : GlyphBase
+    {
+        new MeshRenderer renderer;
+
+        void Awake()
+        {
+            renderer = GetComponent<MeshRenderer>();
+        }
+
+        public override void ApplyColor(Color color)
+        {
+            renderer.material.color = color;
+        }
+    }
+}
