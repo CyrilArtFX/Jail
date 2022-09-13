0a5dbc80f1b74c282a54090de619c76105709511
Guthen <33220603+Guthen@users.noreply.github.com>
2022-09-13 16:37:06 +0200

    [added] camera areas & manager

---
+++ Jail/Assets/Assets/Scripts/Utility/Camera/CameraArea.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+namespace Jail.Utility.Camera
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class CameraArea : MonoBehaviour
+    {
+        public CinemachineVirtualCamera Camera => cvCamera;
+        public BoxCollider Collider { get; private set; }
+
+        [SerializeField]
+        LayerMask triggerMask;
+
+        [SerializeField]
+        CinemachineVirtualCamera cvCamera;
+
+        void Awake()
+        {
+            Collider = GetComponent<BoxCollider>();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (!LayerMaskUtils.HasLayer(triggerMask, other.gameObject.layer)) return;
+
+            CameraManager.SwitchArea(this);
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (this == CameraManager.CurrentArea)
+            {
+                CameraManager.SwitchArea(CameraManager.PreviousArea);
+                print("CameraArea: detected coming back in previous area, switching back..");
+            }
+        }
+
+        void OnDrawGizmos()
+        {
+            if (Collider == null)
+            {
+                Awake();
+            }
+            
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(Collider.bounds.center, Collider.bounds.size);
+        }
+    }
+}
---
+++ Jail/Assets/Assets/Scripts/Utility/Camera/CameraManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Jail.Utility.Camera
+{
+    public sealed class CameraManager : MonoBehaviour
+    {
+        public static CameraArea CurrentArea { get; private set; }
+        public static CameraArea PreviousArea { get; private set; }
+        public static CameraManager instance;
+
+        void Awake()
+        {
+            instance = this;
+        }
+
+        public static void SwitchArea(CameraArea area)
+        {
+            //  switch area
+            PreviousArea = CurrentArea;
+            CurrentArea = area;
+
+            //  switch priority
+            if (PreviousArea != null)
+            {
+                PreviousArea.Camera.Priority = 10;
+            }
+            CurrentArea.Camera.Priority = 11;
+        }
+    }
+}
