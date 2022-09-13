1f152b5fac1bbdf348a7abab67398548855cedb4
CyrilArtFX <ccrozat@artfx.fr>
2022-09-13 13:45:57 +0200

    [added] functions of main menu

---
+++ Jail/Assets/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Jail.Utility;
+
+namespace Jail.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+
+        public void Play()
+        {
+            SceneSwitcher.SwitchScene(1);
+        }
+    }
+}
