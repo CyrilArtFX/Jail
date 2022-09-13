7e2b0377646814299e24df92d9595a41e973de93
CyrilArtFX <ccrozat@artfx.fr>
2022-09-13 20:11:43 +0200

    [fixed] event system now switch properly between mouse and controller

--- Jail/Assets/Assets/Scripts/Menus/EventSystemPlus.cs
+++ Jail/Assets/Assets/Scripts/Menus/EventSystemPlus.cs
@@ -8,6 +8,12 @@ public class EventSystemPlus : MonoBehaviour
     EventSystem eventSystem = default;
     GameObject selectedButton;
 
+    bool isControllerMode;
+    Vector3 previousMousePos;
+
+    [SerializeField]
+    GameObject firstSelectedButton;
+
     void Awake()
     {
         eventSystem = GetComponent<EventSystem>();
@@ -15,10 +21,35 @@ public class EventSystemPlus : MonoBehaviour
 
     void Start()
     {
-        selectedButton = eventSystem.currentSelectedGameObject;
+        selectedButton = firstSelectedButton;
+        isControllerMode = true;
+
+        previousMousePos = Vector3.zero;
     }
 
     void Update()
     {
+        if (isControllerMode)
+        {
+            if (Input.mousePosition != previousMousePos || Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                if (eventSystem.currentSelectedGameObject != null)
+                {
+                    selectedButton = eventSystem.currentSelectedGameObject;
+                }
+                eventSystem.SetSelectedGameObject(null);
+                isControllerMode = false;
+            }
+        }
+        else
+        {
+            if (Input.GetAxis("ControllerX") != 0 || Input.GetAxis("ControllerY") != 0)
+            {
+                eventSystem.SetSelectedGameObject(selectedButton);
+                isControllerMode = true;
+            }
+        }
+
+        previousMousePos = Input.mousePosition;
     }
 }
