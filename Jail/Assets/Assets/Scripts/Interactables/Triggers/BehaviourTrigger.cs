0d28fb61bdf07d5ec89f6c86cce066743e8260a2
Guthen <33220603+Guthen@users.noreply.github.com>
2022-09-14 15:21:56 +0200

    [added] BehaviourTrigger

---
+++ Jail/Assets/Assets/Scripts/Interactables/Triggers/BehaviourTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+using Jail.Utility;
+
+namespace Jail.Interactables.Triggers
+{
+    [Flags]
+    public enum BehaviourTriggerActions : byte
+    {
+        StartDisable = 1,
+        DisableOnExit = 2,
+    }
+
+    public class BehaviourTrigger : BaseTrigger
+    {
+        [SerializeField]
+        BehaviourTriggerActions actions;
+
+        [SerializeField]
+        Behaviour behaviour;
+
+        void Start()
+        {
+            if (LayerMaskUtils.HasFlag((int) actions, (int) BehaviourTriggerActions.StartDisable))
+            {
+                behaviour.enabled = false;
+            }
+        }
+
+        protected override void OnTrigger(Collider other, bool is_enter)
+        {
+            if (is_enter)
+            {
+                behaviour.enabled = true;
+            }
+            else
+            {
+                if (LayerMaskUtils.HasFlag((int) actions, (int) BehaviourTriggerActions.DisableOnExit))
+                {
+                    behaviour.enabled = false;
+                }
+            }
+        }
+    }
+}
