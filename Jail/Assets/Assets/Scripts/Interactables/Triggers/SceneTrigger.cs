8a39043222f5162a0c757f307f516ca451c2575b
Guthen <33220603+Guthen@users.noreply.github.com>
2022-09-15 11:22:37 +0200

    [added] SceneTrigger

---
+++ Jail/Assets/Assets/Scripts/Interactables/Triggers/SceneTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+using Jail.Utility;
+using UnityEngine.SceneManagement;
+
+namespace Jail.Interactables.Triggers
+{
+    public class SceneTrigger : BaseTrigger
+    {
+        [Header("Scene"), Scene, SerializeField]
+        string sceneName;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            color = Color.green;
+        }
+
+        protected override void OnTrigger(Collider other, bool is_enter)
+        {
+            SceneSwitcher.SwitchScene(sceneName);
+        }
+    }
+}
